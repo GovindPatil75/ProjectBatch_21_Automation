025cbcb986d72a8b99df104b4ecefd845019832d
GovindPatil75 <mulegr75@gmail.com>
2023-09-01 09:13:49 +0530

    session Automation 8

--- src/test/java/com/Test/Test_S8.java
+++ src/test/java/com/Test/Test_S8.java
@@ -11,13 +11,17 @@ public class Test_S8 {
 
 	public static void main(String[] args) {
 		
+		//Browser Open
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver=new ChromeDriver();
 		driver.get("https://demo.guru99.com/test/newtours/register.php");
 		driver.manage().window().maximize();
 		
+		//Identify DropDown Element
 		WebElement DrpCountry=driver.findElement(By.xpath("//select[@name='country']"));
 		
+		/*
+		//Object created for Select Class
 		Select select=new Select(DrpCountry);
 		
 		String DeafultOptionSelected=select.getFirstSelectedOption().getText();
@@ -27,6 +31,40 @@ public class Test_S8 {
 		//selectByIndex -int index
 		select.selectByIndex(15);
 		
+		//selectByValue
+		select.selectByValue("AMERICAN SAMOA");
+		
+		select.selectByVisibleText("ANTARCTICA");
+		*/
+		
+		handleDropDown_VisibleText(DrpCountry,"ANTARCTICA");
+		
+		handleDropDown_Value(DrpCountry,"AMERICAN SAMOA");
+		
+		handleDropDown_Index(DrpCountry,10);
+		
+			
+	}
+	
+	public static void handleDropDown_VisibleText(WebElement element,String text) {
+		
+		Select select=new Select(element);
+		select.selectByVisibleText(text);
+	
+	}
+	
+    public static void handleDropDown_Value(WebElement element,String value) {
+		
+		Select select=new Select(element);
+		select.selectByValue(value);
+	
+	}
+    
+   public static void handleDropDown_Index(WebElement element,int index) {
+		
+		Select select=new Select(element);
+		select.selectByIndex(index);
+	
 	}
 
 }
---
+++ src/test/java/com/Test/Test_S9.java
@@ -0,0 +1,47 @@
+package com.Test;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test_S9 {
+
+	public static void main(String[] args) {
+		
+		//Browser Open
+				WebDriverManager.chromedriver().setup();
+				ChromeDriver driver=new ChromeDriver();
+				driver.get("https://demo.guru99.com/test/newtours/register.php");
+				driver.manage().window().maximize();
+				
+				//Identify DropDown Element
+				WebElement DrpCountry=driver.findElement(By.xpath("//select[@name='country']"));
+				
+				Test_S8.handleDropDown_Index(DrpCountry, 9);
+				//Test_S8.handleDropDown_Value(DrpCountry, null);
+				//Test_S8.handleDropDown_VisibleText(DrpCountry, null);
+				
+				Select select=new Select(DrpCountry);
+				List<WebElement> list=select.getOptions();
+				
+				System.out.println(list.size()); // 264
+				
+				for(int i=0;i<list.size();i++) {
+					
+					String text=list.get(i).getText();
+					System.out.println(text);
+					
+					if(text.equals("BARBADOS")) {
+						list.get(i).click();
+						break;
+					}
+				}
+
+	}
+
+}
