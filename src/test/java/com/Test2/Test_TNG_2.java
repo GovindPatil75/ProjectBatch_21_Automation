69546923b9662bbe597104038b11e2477177e02e
GovindPatil75 <86549802+GovindPatil75@users.noreply.github.com>
2023-09-21 09:22:00 +0530

    Update Test_TNG_2.java

--- src/test/java/com/Test2/Test_TNG_2.java
+++ src/test/java/com/Test2/Test_TNG_2.java
@@ -1,5 +1,15 @@
 package com.Test2;
 
 public class Test_TNG_2 {
-
+    WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("");
+		driver.manage().window().maximize();
+		
+		// Element -- identify 
+		
+		driver.findElement(By.xpath("")).sendKeys("");
+		driver.findElement(By.xpath("")).sendKeys("");
+		driver.findElement(By.xpath("")).click();
+		
 }
